943b91332a72bf15b6a7171ca60c16f828910792
ingGibran <zaga1709@hotmail.com>
2026-02-13 15:02:19 -0600

    email

--- api/main.py
+++ api/main.py
@@ -13,7 +13,9 @@ origins = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
     "http://localhost:5173",
-    "http://127.0.0.1:5173"
+    "http://127.0.0.1:5173",
+    "http://localhost:8000",
+    "http://127.0.0.1:8000"
 ]
 
 allowed_origins_env = os.getenv("ALLOWED_ORIGINS")
